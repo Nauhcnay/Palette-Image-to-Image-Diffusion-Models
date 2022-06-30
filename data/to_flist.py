8b9e4a4b098b2fd2e2e4608aca50174d624884cd
Chuan <nauhcnay@gmail.com>
2022-06-30 16:48:59 -0400

    bug fix

--- data/to_flist.py
+++ data/to_flist.py
@@ -1,5 +1,7 @@
 import os
 import cv2
+import random
+
 dataset_path = "../datasets/udf/train"
 flist = []
 for img in os.listdir(dataset_path):
@@ -8,6 +10,12 @@ for img in os.listdir(dataset_path):
     if a.shape[0] < 288 or a.shape[1] < 288:
         continue
     flist.append(os.path.abspath(p))
+
+random.shuffle(flist)
+train_flist = flist[:-32]
+test_flist = flist[-32:]
 with open("../datasets/udf/train.flist", 'w') as f:
-    f.write("\n".join(flist))
+    f.write("\n".join(train_flist))
+with open("../datasets/udf/test.flist", 'w') as f:
+    f.write("\n".join(test_flist))
 print("Done")
