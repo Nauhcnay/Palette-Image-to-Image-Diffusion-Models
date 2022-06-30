b0caaf1ffd8b14b3048000343db4d0e91c661bd1
Chuan <nauhcnay@gmail.com>
2022-06-30 14:25:42 -0400

    bugfix

--- data/to_flist.py
+++ data/to_flist.py
@@ -1,8 +1,13 @@
 import os
+import cv2
 dataset_path = "../datasets/udf/train"
 flist = []
 for img in os.listdir(dataset_path):
-    flist.append(os.path.abspath(os.path.join(dataset_path, img)))
+    p = os.path.join(dataset_path, img)
+    a = cv2.imread(p)
+    if a.shape[0] < 288 or a.shape[1] < 288:
+        continue
+    flist.append(os.path.abspath(p))
 with open("../datasets/udf/train.flist", 'w') as f:
     f.write("\n".join(flist))
 print("Done")
