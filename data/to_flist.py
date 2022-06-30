7d299466d593e881304b03dd5dba81075c91c49a
Chuan <nauhcnay@gmail.com>
2022-06-30 12:45:29 -0400

    path update

--- data/to_flist.py
+++ data/to_flist.py
@@ -1,8 +1,8 @@
 import os
-dataset_path = "./udf/train"
+dataset_path = "../datasets/udf/train"
 flist = []
 for img in os.listdir(dataset_path):
     flist.append(os.path.abspath(os.path.join(dataset_path, img)))
-with open("train.flist", 'w') as f:
+with open("../datasets/udf/train.flist", 'w') as f:
     f.write("\n".join(flist))
 print("Done")
