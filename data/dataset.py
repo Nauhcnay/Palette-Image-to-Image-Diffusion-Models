764d2e7c4ec6974672cc6fe6a9c852b7f204bf69
Chuan Yan <nauhcnay@gmail.com>
2022-10-09 03:17:35 -0400

    bug fix

--- data/dataset.py
+++ data/dataset.py
@@ -304,7 +304,7 @@ class ShadingDataset(data.Dataset):
         shad_np = shad_np.clip(0, 255)
 
         # to tensors
-        label = torch.Tensor([label]).expand(1, self.flat_np.shape[0], self.flat_np.shape[1])
+        label = torch.Tensor([label]).expand(1, flat_np.shape[0], flat_np.shape[1])
         flat = self.to_tensor(flat_np / 255)
         flat = torch.cat((flat, label), dim = 0)
         shad = self.to_tensor(1 - shad_np / 255)
